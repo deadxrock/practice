de257c8ae8e3c2320efddf44df5e059b4fcc9471
Dasha Prichina <dashaprichina@gmail.com>
2025-03-31 01:03:22 +0300

    Update pr4_2.py

--- classwork/pr4_2.py
+++ classwork/pr4_2.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+data = pd.Series(np.random.randint(51, 100, size=10))
+mean_value = data.mean() # Обчислюємо середнє арифметичне
+greater_than_mean = data[data > mean_value]
+greater_than_mean_series = pd.Series(greater_than_mean) # Переписуємо їх в інший масив Series
 
+print("Оригінальний масив:")
+print(data)
+print("\nСереднє арифметичне:", mean_value)
+print("\nЧисла, що більше за середнє арифметичне:")
+print(greater_than_mean_series)
